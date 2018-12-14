e145a89f077c1bc87a3419a43d30143062a57ca6
Manan Gupta <manan17372@iiitd.ac.in>
2018-12-14 17:22:03 +0530

    labels added
    labels added to the edge and vertex. model needs to be optimized so that they do not get in the way.

--- edge.java
+++ edge.java
@@ -1,6 +1,7 @@
 package DijkstraVisualization;
 
 import javafx.scene.Group;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
@@ -8,17 +9,27 @@ public class edge {
 	Line l;
 	vertex a, b;
 	double weight;
+	Label text;
 
 	// TODO Add a label to mark the weight of the line
 	
 	public edge(vertex u, vertex v) {
 		l = new Line(u.x, u.y, v.x, v.y);
+		double xcenter = u.x/2 + v.x/2;
+		double ycenter = u.y/2 + v.y/2;
+		
 		weight = Math.sqrt(Math.pow(u.x - v.x, 2) + Math.pow(u.y - v.y, 2));
+		
+		text = new Label(String.format("%.2f", weight));
+		text.setTranslateX(xcenter-45);
+		text.setTranslateY(ycenter-30);
+		
 //		System.out.println(weight);
 	}
 
 	public void showEdge(Group g) {
 		g.getChildren().add(l);
+		g.getChildren().add(text);
 	}
 
 	public void addToMinSpanTree() {
--- vertex.java
+++ vertex.java
@@ -3,26 +3,33 @@ package DijkstraVisualization;
 import java.util.LinkedList;
 
 import javafx.scene.Group;
+import javafx.scene.control.Label;
 import javafx.scene.shape.Circle;
 
 public class vertex {
+	static char alphabet = 'A';
 	Circle c;
 	double x;
 	double y;
 	LinkedList<edge> adjEdges = new LinkedList<>();
-
+	Label text;
 	// TODO Add a label to mark the weight of the vertex
 	
 	public vertex(double x2, double y2) {
 		x = x2;
 		y = y2;
 		c = new Circle(x, y, 3);
-		c.setId("manan");
+		c.setId(Character.toString(alphabet));
+		text = new Label(c.getId() + " [ "+x+", "+y+"]");
+		text.setTranslateX(x-45);
+		text.setTranslateY(y-30);
 		System.out.println(c.getId());
+		alphabet++;
 	}
 
 	public void showVertex(Group g) {
 		g.getChildren().add(c);
+		g.getChildren().add(text);
 	}
 
 	public void addEdge(edge e) {
@@ -39,6 +46,9 @@ public class vertex {
 			}
 		}
 		x = x2;
+		text.setText("[ "+x+", "+y+"]");
+		text.setTranslateX(x-45);
+		text.setTranslateY(y-30);
 	}
 	
 	public void updateY(double y2) {
@@ -51,5 +61,9 @@ public class vertex {
 			}
 		}
 		y = y2;
+
+		text.setText("[ "+x+", "+y+"]");
+		text.setTranslateX(x-45);
+		text.setTranslateY(y-30);
 	}
 }
